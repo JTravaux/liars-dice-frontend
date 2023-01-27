c9859458da2a3479101d0c07cc0d93f98d2bbc9a
Jordan <jordan.travaux@hotmail.com>
2023-01-27 14:59:28 -0500

    use yarn, remove unneeded packages

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { SnackbarProvider } from 'notistack';
 import { BASE_PATH } from './util/constants';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { ConfigProvider } from './context/ConfigContext';
 
@@ -21,8 +20,3 @@ root.render(
         </SnackbarProvider>
     </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
